4a221bf1fe9c524b72403894e430da7cf4270d71
Rohith2201 <julururohith191020109@gmail.com>
2025-10-25 11:50:20 +0000

    Repository and service

--- springapp/src/main/java/com/examly/springapp/model/User.java
+++ springapp/src/main/java/com/examly/springapp/model/User.java
@@ -1,5 +1,13 @@
 package com.examly.springapp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue
     private long userId;
     private String email;
     private String password;
--- springapp/src/main/java/com/examly/springapp/service/UserService.java
+++ springapp/src/main/java/com/examly/springapp/service/UserService.java
@@ -1,4 +1,14 @@
 package com.examly.springapp.service;
+
+import com.examly.springapp.repository.UserRepo;
+
 public class UserService {
+
+    private UserRepo uRepo;
+
+    public UserService(UserRepo uRepo) {
+        this.uRepo = uRepo;
+    }
+    
     
 }
