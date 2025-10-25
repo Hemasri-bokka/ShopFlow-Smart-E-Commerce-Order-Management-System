3dcec8c89806ef3d2e4a532044e556ccbce50026
Rohith2201 <julururohith191020109@gmail.com>
2025-10-25 12:04:42 +0000

    Updated Service

--- springapp/src/main/java/com/examly/springapp/service/UserServiceImpl.java
+++ springapp/src/main/java/com/examly/springapp/service/UserServiceImpl.java
@@ -1,12 +1,13 @@
 package com.examly.springapp.service;
+
 import com.examly.springapp.repository.UserRepo;
 
 public class UserServiceImpl {
 
-        private UserRepo uRepo;
+    private UserRepo uRepo;
 
     public UserServiceImpl(UserRepo uRepo) {
         this.uRepo = uRepo;
     }
-    
+
 }
