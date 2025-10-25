14856374f11e24f1b5a39f436de412ff65a0874b
SahuParul <parulsahu31300@gmail.com>
2025-10-25 12:29:56 +0000

    Product upto Service

---
+++ springapp/src/main/java/com/examly/springapp/service/ProductServiceImpl.java
@@ -0,0 +1,38 @@
+package com.examly.springapp.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.examly.springapp.model.Product;
+@Service
+public class ProductServiceImpl implements ProductService{
+
+    public Product addProduct(Product product){
+
+    }
+
+    public List<Product> getAllProducts(){
+
+    }
+
+    public Product getProductById(Long id){
+
+    }
+
+    public List<Product> getProductByUserId(Long userId){
+
+    }
+
+    public List<Product> getProductsByCategory(String category){
+
+    }
+
+    public void deleteProduct(Long id){
+
+    }
+
+    public Product updateProduct(Long productId, ProductRequest productRequest){
+
+    }
+}
