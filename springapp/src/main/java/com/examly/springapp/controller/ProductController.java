91ced26b1964f037f1679fe663cf7d1f943b05d3
SahuParul <parulsahu31300@gmail.com>
2025-10-27 04:25:04 +0000

    day2 controller remaining

--- springapp/src/main/java/com/examly/springapp/controller/ProductController.java
+++ springapp/src/main/java/com/examly/springapp/controller/ProductController.java
@@ -1,5 +1,16 @@
 package com.examly.springapp.controller;
 
+import com.examly.springapp.service.ProductServiceImpl;
+
 public class ProductController {
-    
+    private ProductServiceImpl service;
+
+    public ProductController(ProductServiceImpl service){
+        this.service = service;
+    }
+
+    @PostMapping("/api/products")
+    public ResponseEntity<Product> addProduct( @RequestBody Product product){
+
+    }
 }
