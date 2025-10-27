b273bbe7f45c2f966bd296266ad0381f332269d4
jeffringladson <jeffringladson07@gmail.com>
2025-10-27 04:32:52 +0000

    Feedback controller done -Gladson

--- springapp/src/main/java/com/examly/springapp/controller/FeedbackController.java
+++ springapp/src/main/java/com/examly/springapp/controller/FeedbackController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,6 +48,13 @@ public class FeedbackController {
         
     }
 
-
+    @DeleteMapping("/api/feedback/{id}")
+    public ResponseEntity<Feedback> delete(@PathVariable long id){
+        Feedback found = ser.deleteFeedback(id);
+        if(found == null){
+            return ResponseEntity.status(404).build();
+        }
+        return ResponseEntity.status(200).body(found); 
+    }
 
 }
