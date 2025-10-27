176adbbfd2100350389877a81aa846ef405f7280
SahuParul <parulsahu31300@gmail.com>
2025-10-27 13:23:04 +0000

    one method for testing

--- springapp/src/main/java/com/examly/springapp/config/MyUserDetailsService.java
+++ springapp/src/main/java/com/examly/springapp/config/MyUserDetailsService.java
@@ -1,35 +1,35 @@
-// package com.examly.springapp.config;
-// import java.util.Optional;
+package com.examly.springapp.config;
+import java.util.Optional;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.security.core.userdetails.UserDetails;
-// import org.springframework.security.core.userdetails.UserDetailsService;
-// import org.springframework.security.core.userdetails.UsernameNotFoundException;
-// import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
-// import com.examly.springapp.model.User;
-// import com.examly.springapp.repository.UserRepo;
+import com.examly.springapp.model.User;
+import com.examly.springapp.repository.UserRepo;
 
 
-// public class MyUserDetailsService implements UserDetailsService{
-//     private UserRepo uRepo;
+public class MyUserDetailsService implements UserDetailsService{
+    private UserRepo uRepo;
     
     
-//     public MyUserDetailsService(UserRepo uRepo) {
-//         this.uRepo = uRepo;
-//     }
+    public MyUserDetailsService(UserRepo uRepo) {
+        this.uRepo = uRepo;
+    }
 
 
-//     @Override
-//     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//         User user = uRepo.findByUsername(username).orElse(null);
-//         if(user == null){
-//             throw new UsernameNotFoundException("Username does not exist");
-//         }
-//         return new UserPrinciple(user);
-//     }
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = uRepo.findByUsername(username).orElse(null);
+        if(user == null){
+            throw new UsernameNotFoundException("Username does not exist");
+        }
+        return new UserPrinciple(user);
+    }
     
-// }
+}
 
 
 
