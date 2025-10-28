0e3448145e4e7ef365e046c0ae01c8a2cee899f6
jeffringladson <jeffringladson07@gmail.com>
2025-10-28 11:21:02 +0000

    Done some angular

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AdminaddproductComponent } from './components/adminaddproduct/adminaddproduct.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AdminaddproductComponent
   ],
   imports: [
     BrowserModule,
