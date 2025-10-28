0a2b46cf8f9e6602956cdcc056ba49b8d6ce1dbe
Rohith2201 <julururohith191020109@gmail.com>
2025-10-28 12:22:44 +0000

    Few

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -3,15 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { ErrorComponent } from './error/error.component';
+import { HomeComponent } from './components/home/home.component';
+import { ErrorComponent } from './components/error/error.component';
 import { AdminviewproductComponent } from './components/adminviewproduct/adminviewproduct.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ErrorComponent
+    ErrorComponent,
     AdminviewproductComponent
   ],
   imports: [
