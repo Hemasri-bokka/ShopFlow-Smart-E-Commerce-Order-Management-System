7e7d66e3854b267f975bd83303f9bb4b8aeafeba
Rohith2201 <julururohith191020109@gmail.com>
2025-10-29 11:07:29 +0000

    Day 4 angular

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { ErrorComponent } from './components/error/error.component';
+
 import { AdminviewproductComponent } from './components/adminviewproduct/adminviewproduct.component';
 import { AdminnavbarComponent } from './components/adminnavbar/adminnavbar.component';
 import { AdminviewordersComponent } from './components/adminvieworders/adminvieworders.component';
 import { AdminviewuserdetailsComponent } from './components/adminviewuserdetails/adminviewuserdetails.component';
+
 import { AuthguardComponent } from './components/authguard/authguard.component';
 import { LoginComponent } from './components/login/login.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -19,6 +29,9 @@ import { UsernavbarComponent } from './components/usernavbar/usernavbar.componen
 import { UserviewfeedbackComponent } from './components/userviewfeedback/userviewfeedback.component';
 import { UserviewordersComponent } from './components/uservieworders/uservieworders.component';
 import { UserviewproductComponent } from './components/userviewproduct/userviewproduct.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -38,11 +51,23 @@ import { UserviewproductComponent } from './components/userviewproduct/userviewp
     UsernavbarComponent,
     UserviewfeedbackComponent,
     UserviewordersComponent,
-    UserviewproductComponent
+    UserviewproductComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatIconModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angularapp/src/app/components/registration/registration.component.ts
+++ angularapp/src/app/components/registration/registration.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user.model';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-registration',
@@ -6,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
+  registerForm:FormGroup
+  users:User[] = [];
+  constructor(private fb:FormBuilder,private service:AuthService,private router:Router){
+    this.registerForm = this.fb.group({
+      username:['',Validators.required],
+      email:['',Validators.required],
+      password:['',Validators.required],
+      confirmPassword:['',Validators.required],
+      mobileNumber:['',Validators.required],
+      userRole:['',Validators.required],
+    })
+  }
+  onSubmit(){
+    if(this.registerForm.valid){
+      this.service.register(this.registerForm.value).subscribe((result)=>{
+        this.registerForm.reset()
+        alert("Registration successful")
+        this.router.navigate(['/login'])
+      },
+      (error)=>{
+        this.registerForm.reset()
+        alert("Registration not done")
+        this.router.navigate(['/error'],{queryParams:{errorMsg:'Registration not done due to exising emailId'}} )
+      });
 
-  constructor() { }
-
+    }
+  }
   ngOnInit(): void {
   }
+
 
 }
