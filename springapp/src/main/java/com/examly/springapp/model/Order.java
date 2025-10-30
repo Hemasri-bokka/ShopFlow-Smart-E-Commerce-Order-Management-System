7efc704c3e83e2b6ed7fa70d6724708f440f3af0
Rohith2201 <julururohith191020109@gmail.com>
2025-10-30 07:53:05 +0000

    View order changes

--- springapp/src/main/java/com/examly/springapp/model/Order.java
+++ springapp/src/main/java/com/examly/springapp/model/Order.java
@@ -48,12 +48,26 @@ public class Order {
             public Order() {
             }
 
-    public Order(long orderId, String shippingAddress, double totalAmount, int quantity, Date createdAt, Date updatedAt,
-            User user, List<Product> product) {
+    // public Order(long orderId, String shippingAddress, double totalAmount, int quantity, Date createdAt, Date updatedAt,
+    //         User user, List<Product> product) {
+    //     this.orderId = orderId;
+    //     this.shippingAddress = shippingAddress;
+    //     this.totalAmount = totalAmount;
+    //     this.quantity = quantity;
+    //     this.createdAt = createdAt;
+    //     this.updatedAt = updatedAt;
+    //     this.user = user;
+    //     this.product = product;
+    // }
+    
+
+    public Order(long orderId, String shippingAddress, double totalAmount, int quantity, OrderStatus status,
+            Date createdAt, Date updatedAt, User user, List<Product> product) {
         this.orderId = orderId;
         this.shippingAddress = shippingAddress;
         this.totalAmount = totalAmount;
         this.quantity = quantity;
+        this.status = status;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
         this.user = user;
@@ -122,6 +136,14 @@ public class Order {
 
     public void setProduct(List<Product> product) {
         this.product = product;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
     }
 
     
