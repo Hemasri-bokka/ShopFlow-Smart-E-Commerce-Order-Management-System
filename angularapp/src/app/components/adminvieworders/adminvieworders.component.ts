b461cc689b8e95d4c02e0543b6a59e4ec154f486
Rohith2201 <julururohith191020109@gmail.com>
2025-10-31 03:42:12 +0000

    Material UI

--- angularapp/src/app/components/adminvieworders/adminvieworders.component.ts
+++ angularapp/src/app/components/adminvieworders/adminvieworders.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { Order } from 'src/app/models/order.model';
 import { OrderService } from 'src/app/services/order.service';
 
@@ -9,49 +13,68 @@ import { OrderService } from 'src/app/services/order.service';
 })
 export class AdminviewordersComponent implements OnInit {
 
-  constructor(private ser : OrderService) { }
-  orders: Order[] = [];
-  statusOptions: string[] = ['PENDING', 'ACCEPTED', 'PACKED', 'SHIPPED', 'DELIVERED', 'OUT FOR DELIVERY', 'CANCEL', 'RETURN'];
+  constructor(private ser: OrderService, private snackBar: MatSnackBar) { }
+  dataSource = new MatTableDataSource<Order>();
+  // statusOptions: string[] = ['PENDING', 'ACCEPTED', 'PACKED', 'SHIPPED', 'DELIVERED', 'OUT FOR DELIVERY', 'CANCEL', 'RETURN'];
+  statusOptions: string[] = ['pending', 'accepted', 'packed', 'shipped', 'delivered', 'out for delivery', 'cancel', 'return', 'processing'];
+  displayedColumns: string[] = [
+    'sno', 'orderId', 'username', 'productName', 'price', 'shippingAddress', 'totalAmount', 'quantity', 'status'
+  ];
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+
 
   ngOnInit(): void {
     this.getOrders();
   }
 
-  getOrders(){
+
+  getOrders() {
     this.ser.getOrders().subscribe(data => {
-      this.orders = data;
-    })
-
-    // this.orders = [
-    //   {
-    //     orderId: 1,
-    //     user: {
-    //       userId: 101,
-    //       username: 'test_user'
-    //     },
-    //     product: [
-    //       {
-    //         productId: 201,
-    //         name: 'Sample Product',
-    //         price: 100
-    //       }
-    //     ],
-    //     shippingAddress: 'Bangalore',
-    //     totalAmount: 100,
-    //     quantity: 1,
-    //     status: 'PENDING',
-    //     createdAt: new Date(),
-    //     updatedAt: new Date()
-    //   }
-    // ];
-
+      this.dataSource.data = data;
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    });
   }
 
   updateStatus(order: Order): void {
-    this.ser.updateOrderStatus(order.orderId, order.status).subscribe({
-      next: () => console.log(`Order ${order.orderId} status updated to ${order.status}`),
-      error: (err) => console.error('Error updating status:', err)
+    this.ser.updateOrderStatus(order.orderId, order).subscribe({
+      next: () => {
+        this.snackBar.open(`Order ${order.orderId} status updated to ${order.status}`, 'Close', {
+          duration: 3000,
+          panelClass: ['snackbar-success'],
+          horizontalPosition: 'right',
+          verticalPosition: 'top'
+
+        });
+      },
+      error: (err) => {
+        console.error('Error updating status:', err);
+        this.snackBar.open('Failed to update order status. Please try again.', 'Close', {
+          duration: 3000,
+          panelClass: ['snackbar-error'],
+          horizontalPosition: 'right',
+          verticalPosition: 'top'
+        });
+      }
     });
   }
+
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value.trim().toLowerCase();
+    this.dataSource.filter = filterValue;
+  }
+
+  filterByStatus(status: string) {
+    this.dataSource.filter = status.trim().toLowerCase();
+  }
+
+
+
+
+
 }
 
--- angularapp/src/app/components/registration/registration.component.ts
+++ angularapp/src/app/components/registration/registration.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { User } from 'src/app/models/user.model';
 import { AuthService } from 'src/app/services/auth.service';
@@ -12,30 +13,52 @@ import { AuthService } from 'src/app/services/auth.service';
 export class RegistrationComponent implements OnInit {
   registerForm:FormGroup
   users:User[] = [];
-  constructor(private fb:FormBuilder,private service:AuthService,private router:Router){
+  constructor(private fb:FormBuilder,private service:AuthService,private router:Router, private snackBar: MatSnackBar){
 
     this.registerForm = this.fb.group({
       username:['',Validators.required],
       email:['',Validators.required],
-      password:['',Validators.required],
+      password: ['',[Validators.required,Validators.pattern(/^(?=.*[0-9])(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{6,}$/)]],
       confirmPassword:['',Validators.required],
       mobileNumber:['',Validators.required],
       userRole:['',Validators.required],
-    })
+    },  {validators: this.passwordMatchValidator })
   }
-  onSubmit(){
-    if(this.registerForm.valid){
-      this.service.register(this.registerForm.value).subscribe((result)=>{
-        this.registerForm.reset()
-        alert("Registration successful")
-        this.router.navigate(['/login'])
-      },
-      (error)=>{
-        this.registerForm.reset()
-        alert("Registration not done")
-        this.router.navigate(['/error'],{queryParams:{errorMsg:'Registration not done due to exising Username'}} )
-      });
+  passwordMatchValidator(form: FormGroup) {
+    const password = form.get('password')?.value;
+    const confirmPassword = form.get('confirmPassword')?.value;
+    return password === confirmPassword ? null : { mismatch: true };
+  }
+  // onSubmit(){
+  //   if(this.registerForm.valid){
+  //     this.service.register(this.registerForm.value).subscribe((result)=>{
+  //       this.registerForm.reset()
+  //       alert("Registration successful")
+  //       this.router.navigate(['/login'])
+  //     },
+  //     (error)=>{
+  //       this.registerForm.reset()
+  //       alert("Registration not done")
+  //       this.router.navigate(['/error'],{queryParams:{errorMsg:'Registration not done due to exising Username'}} )
+  //     });
 
+  //   }
+  // }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      this.service.register(this.registerForm.value).subscribe(
+        (result) => {
+          this.registerForm.reset();
+          this.snackBar.open('Registration successful', 'Close', { duration: 3000, panelClass: ['success-snackbar'] });
+          this.router.navigate(['/login']);
+        },
+        (error) => {
+          this.registerForm.reset();
+          this.snackBar.open('Registration not done due to existing Username', 'Close', { duration: 4000, panelClass: ['error-snackbar'] });
+          // this.router.navigate(['/error'], { queryParams: { errorMsg: 'Registration not done due to existing Username' } });
+        }
+      );
     }
   }
   ngOnInit(): void {
