baee042f39c2cc9842743f9f8de5e452d6e22382
its-prajith <prajithcbe@gmail.com>
2025-11-04 10:59:57 +0000

    Home page Overlay issue Resolved

--- angularapp/src/app/app.constants.ts
+++ angularapp/src/app/app.constants.ts
@@ -1,3 +1,4 @@
-// export const APP_URL = 'https://8080-dbdaddbdaeedadbddffabbccfeabfadfbfdec.premiumproject.examly.io/api';
+export const APP_URL = 'https://8080-dbdaddbdaeedadbddffabbccfeabfadfbfdec.premiumproject.examly.io/api';
 
-export const APP_URL = 'https://8080-baadeefefccdbfaaaeedadbddffabbccfeabfadfbfdec.premiumproject.examly.io/api';
+// export const APP_URL = 'https://8080-baadeefefccdbfaaaeedadbddffabbccfeabfadfbfdec.premiumproject.examly.io/api';
+// export const APP_URL = 'https://8080-cecbbedbbdcdaeedadbddffabbccfeabfadfbfdec.premiumproject.examly.io/api';
